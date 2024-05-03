3857bf9d3822935bb8c5145b5d2d703736d3f867
chgw <chgw1012@naver.com>
2024-05-03 09:50:57 +0900

    자리이동 후 첫 커밋

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,15 +7,30 @@ const ExpenseItem = ({ title, price, date }) => {
   // 숫자를 화폐 표기법으로 바꾸기
   const formattedPrice = new Intl.NumberFormat('ko-KR').format(price);
 
+  const clickHandler = (e) => {
+    console.log('클릭이벤트발생!');
+  };
+
   return (
-    <Card className="circle">
-      <div className="expense-item">
+    <Card className='circle'>
+      <div className='expense-item'>
         <ExpenseDate date={date} />
-        <div className="expense-item__description">
+        <div className='expense-item__description'>
           <h2>{title}</h2>
-          <div className="expense-item__price">{formattedPrice}원</div>
+          <div className='expense-item__price'>{formattedPrice}원</div>
         </div>
       </div>
+      <button id='btn1' onClick={clickHandler}>
+        수정
+      </button>
+      <button
+        id='btn2'
+        onClick={(e) => {
+          console.log('삭제 버튼 클릭됨!');
+        }}
+      >
+        삭제
+      </button>
     </Card>
   );
 };
